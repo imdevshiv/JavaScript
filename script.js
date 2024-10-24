929a6ecae36275ac2100695443b59943e3fecd7b
SHIV <72968797+imdevshiv@users.noreply.github.com>
2024-10-24 21:59:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// What is JavaScript?
+/**Ans:- JavaScript is a programming language which make website dynamic or we can say add functionality to a website .
+ *       ->Its helps in CSR(Client side Rendering || browser)
+ *
+ *      CSR(Client side Rendering):-is a technique where the browser is responsible for rendering the content.
+ *                                  When a user visits a website, the server sends minimal HTML along with JavaScript files,
+ *                                  which the browser then executes to dynamically build the page content.
+ *
+ *      SSR(Server Side Rendering):-is a technique used to render web pages on the server instead of in the browser.
+ *                                   When a user requests a page, the server generates the complete HTML for that page
+ *                                     and sends it to the browser.
+ *
+ * HISTORY OF Js
+ * Ans:- NetScape a popular web browser in 90's decided to utilize the power of the machine on which a the browser is install .
+ *          So, the asked Brendan Eich to create a scripting language for browser to use the computational power of a computer .
+ *          So, that website can be dynamic prior to that website used to be static ().
+ *          In 1995 (Birth of JavaScript)(original name was Mocha, the LiveScript and then to JavaScript(obv. for marketing))
+ *          In 1996 (JavaScript vs JScript(microsoft))
+ *          In 1997 (ECMAScript Standardization)(ECMA:-European Computer Manufacturers Association)
+ *
+ *          In 2009 (ES5 and Modern JavaScript)( JSON support, "strict mode")
+ *          In 2015(ES6/ES2015 and the Modern Era)(arrow functions, classes, modules, let/const, and Promises)
+ */
+
+console.log("Hello World");
